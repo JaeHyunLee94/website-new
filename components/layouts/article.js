59b6dd3cdd3f9b4e3daa424b9926ca2854740e6d
jhlee <leejaehyun1223@gmail.com>
2025-04-22 01:09:33 -0500

    renamed tab

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Changwoon Choi`
+  const t = `${title} - JaeHyun Lee`
   return (
     <motion.article
       initial="hidden"
